4728bbee032de312b047e785dbc971e3b2c86b02
Elekoot <kootoomo@gmail.com>
2019-03-02 02:49:08 +0300

    DB update Final

--- udemyFlask/app/process_data.py
+++ udemyFlask/app/process_data.py
@@ -1,4 +1,7 @@
 import json
+import sqlite3
+from sqlitedict import SqliteDict 
+
 
 def get_data(query_string):
     import http.client
@@ -16,9 +19,20 @@ def calculate_coin(price_usd):
     return(float(price_usd) > 40)
 
 def process_coins():
-    data = get_data("/v1/ticker/?limit=10")
+    coins_db = SqliteDict('./coins.db', autocommit=True)
+    data = coins_db["coin_data"]
+    coins_db.close()
+
     all_coins = []
     for coin in data:
         coin["isover40"] = calculate_coin(coin["price_usd"])
         all_coins.append(coin)
     return all_coins
+
+def set_coins_json():
+    coins = SqliteDict('../coins.db', autocommit=True)
+    coins["coin_data"] = get_data("/v1/ticker/?limit=10")
+    coins.close()
+
+if __name__ == "__main__":
+    set_coins_json()
